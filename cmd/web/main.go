efb609e9c2ef2f3b23e269a270adc104894d4c9c
Areesh18 <areeshzafar396@gmail.com>
2026-04-09 14:46:12 +0530

    changed hardcoded config settings to runtime flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,29 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	//fmt.Printf("Before Parsing, The value at %p is %s", addr, *addr)
+	flag.Parse()
+	//fmt.Printf("\nAfter Parsing, The value at %p is %s\n", addr, *addr)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	//create a fileserver for serving out static files from ./ui/static
-	fileServer:=http.FileServer((http.Dir("./ui/static")))
+	fileServer := http.FileServer((http.Dir("./ui/static")))
 
 	//strip the "/static" using http.StripPrefix to avoid doubling up like "/static/static/...."
-	mux.Handle("/static/",http.StripPrefix("/static",fileServer))
-	log.Println("Starting server on: 4000")
-	err := http.ListenAndServe(":4000", mux)
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	log.Printf("Starting server on: %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
