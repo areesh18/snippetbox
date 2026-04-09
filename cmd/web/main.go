a1da1c13324b72899eb97d7104583aca43ebd9ab
Areesh18 <areeshzafar396@gmail.com>
2026-04-09 17:42:21 +0530

    isolate routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,10 +7,11 @@ import (
 	"os"
 )
 
-type application struct{
+type application struct {
 	errorLog *log.Logger
-	infoLog *log.Logger
+	infoLog  *log.Logger
 }
+
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
@@ -18,30 +19,20 @@ func main() {
 	flag.Parse()
 	//fmt.Printf("\nAfter Parsing, The value at %p is %s\n", addr, *addr)
 
-	
-
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	app:=&application{
+	app := &application{
 		errorLog: errorLog,
-		infoLog: infoLog,
+		infoLog:  infoLog,
 	}
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	//create a fileserver for serving out static files from ./ui/static
-	fileServer := http.FileServer((http.Dir("./ui/static")))
 
 	//strip the "/static" using http.StripPrefix to avoid doubling up like "/static/static/...."
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 	infoLog.Printf("Starting server on %s", *addr)
 	err := srv.ListenAndServe()
---
+++ cmd/web/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+	//create a fileserver for serving out static files from ./ui/static
+	fileServer := http.FileServer((http.Dir("./ui/static")))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+
+	return mux
+}
