b9a8b6780c71e48f4d66c11233f5bf48e007e9e0
Areesh18 <areeshzafar396@gmail.com>
2026-04-18 13:07:43 +0530

    refactor middleware to use justinas/alice package

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,9 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives.
 
+	standardMiddleware:=alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -12,9 +18,5 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer((http.Dir("./ui/static")))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	// Pass the servemux as the 'next' parameter to the secureHeaders middleware
-	// Because secureHeaders is just a function, and the function returns a
-	// http.Handler we don't need to do anything else.
-
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return standardMiddleware.Then(mux)
 }
