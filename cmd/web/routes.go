69dd38278811ce7a2dc7961edca9ce01f2224ab9
Areesh18 <areeshzafar396@gmail.com>
2026-04-19 09:13:08 +0530

    refactor to restful routing using bmizerany/pat package

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/bmizerany/pat"
 	"github.com/justinas/alice"
 )
 
@@ -9,14 +11,17 @@ func (app *application) routes() http.Handler {
 	// Create a middleware chain containing our 'standard' middleware
 	// which will be used for every request our application receives.
 
-	standardMiddleware:=alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	//updating to use pats
+	mux := pat.New()
+	mux.Get("/", http.HandlerFunc(app.home))
+	mux.Get("/snippet/create", http.HandlerFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", http.HandlerFunc(app.createSnippet))
+	mux.Get("/snippet/:id", http.HandlerFunc(app.showSnippet))
 	//create a fileserver for serving out static files from ./ui/static
 	fileServer := http.FileServer((http.Dir("./ui/static")))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 
 	return standardMiddleware.Then(mux)
 }
