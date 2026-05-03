4b49bc282aead92003ef59a28dde0171c77dbb2b
Areesh18 <areeshzafar396@gmail.com>
2026-05-03 17:26:02 +0530

    C:/Program Files/Git/create,/logout routed secured with authenticat middlewware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -40,3 +40,13 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 
 	})
 }
+func (app *application) requireAuthenticatedUser(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if app.authenticatedUser(r) == 0 {
+			http.Redirect(w, r, "/user/login", 302)
+			return
+		}
+		//otherwise call the next handler in the chain
+		next.ServeHTTP(w, r)
+	})
+}
